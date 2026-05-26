3766368c0f172cda6101bbc71f7c3ba17923599b
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-05-26 10:00:00 +0530

    Supplier

---
+++ backend/src/main/java/com/smartelectro/repository/SupplierRepository.java
@@ -0,0 +1,9 @@
+package com.smartelectro.repository;
+
+import com.smartelectro.model.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+    Optional<Supplier> findByUserId(Long userId);
+}
