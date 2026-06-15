b3aca59a2c923e049d3b4e589c18610b295bd26b
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-06-15 10:00:00 +0530

    User Repo

---
+++ backend/src/main/java/com/smartelectro/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.smartelectro.repository;
+
+import com.smartelectro.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
